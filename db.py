667333c9aafc4fe70415faf2a2519aed04557667
Alex <a>
2022-11-02 02:34:41 +0300

    + db with json format support

--- db.py
+++ db.py
@@ -1,7 +1,8 @@
 import os
 import pickle
 import string
-
+import json
+from dataclass import Post
 
 class DB:
     """
@@ -11,6 +12,23 @@ class DB:
 
     _path_db = f"{os.getcwd()}/db/"
 
+    def __init__(self, name):
+        self.file_name = self.__format_url_to_name(name)
+        self.path_file = self._path_db + self.file_name
+
+        self.data: list = []
+        self.load()
+
+    def __check_exist_file(self):
+        if self.file_name in os.listdir(self._path_db):
+            return True
+        else:
+            return False
+
+    def __format_url_to_name(self, name):
+        return ''.join(filter(lambda l: l in string.ascii_letters, name)).replace('www', '').replace('https', '').replace('http', '')
+
+
     def save(self):
         pass
 
@@ -19,12 +37,6 @@ class DB:
 
 
 class PickleDB(DB):
-    def __init__(self, name):
-        self.file_name = self.__format_url_to_name(name)
-        self.path_file = self._path_db + self.file_name
-
-        self.data: list = []
-        self.load()
 
     def save(self):
         with open(self.path_file, 'wb') as file:
@@ -42,19 +54,37 @@ class PickleDB(DB):
         else:
             open(self.path_file, 'x')
 
-    def __check_exist_file(self):
-        if self.file_name in os.listdir(self._path_db):
-            return True
-        else:
-            return False
-
-    def __format_url_to_name(self, name):
-        return ''.join(filter(lambda l: l in string.ascii_letters, name)).replace('www', '').replace('https', '').replace('http', '')
-
 
 class JsonDB(DB):
-    pass
+    def __init__(self, name):
+        super().__init__(name)
+        self.path_file += ".json"
+
+    def save(self):
+        with open(self.path_file, "w") as file:
+            json.dump(self.format_to_json(), file, indent=4, ensure_ascii=False)
+
+    def load(self):
+        with open(self.path_file, "r") as file:
+            a = json.load(file)
+            self.format_from_json(a)
+
+    def format_to_json(self):
+        dec = []
+        for i in self.data:
+            tmp = [i.title, i.date, i.content, i.url]
+            dec.append(tmp)
+
+        return dec
+
+    def format_from_json(self, ls):
+        self.data = []
+        for i in ls:
+            for j in i:
+                self.data.append(Post(*j))
+
 
 
 def factory_db(*args, **kwargs):
-    return PickleDB(*args, **kwargs)
+    # return PickleDB(*args, **kwargs)
+    return JsonDB(*args, **kwargs)
